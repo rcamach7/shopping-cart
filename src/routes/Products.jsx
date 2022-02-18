aa32d24ff9be3ae089fa36ac90b87995e3c0938d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-18 15:50:04 -0800

    display car data in component

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,5 +1,7 @@
 import "../scss/Products.scss";
 import data from "../data/cars.json";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 function Products() {
@@ -15,17 +17,37 @@ function Products() {
 
 function CarBox(props) {
   const { car } = props;
+
+  const formatCurrency = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
   return (
     <div className="CarBox">
       <div
         className="imageContainer"
         style={{ backgroundImage: `url(${car.imageURL})` }}
-      ></div>
+      >
+        <FontAwesomeIcon icon={faHeart} className="favoriteHeart" />
+      </div>
 
       <div className="carInfo">
         <p className="yearAndMake">{`${car.year} ${car.make}`}</p>
         <p className="model">{car.model}</p>
-        <p className="priceAndMileage">{`$${car.price} * ${car.miles} miles`}</p>
+        <p className="priceAndMileage">{`${formatCurrency.format(
+          car.price
+        )} : ${car.miles}`}</p>
+
+        <div className="action-buttons">
+          <button style={{ backgroundColor: "rgb(130,179,67)" }}>
+            Add To Cart
+          </button>
+          <button style={{ backgroundColor: "rgb(233,109,108)" }}>
+            Remove From Cart
+          </button>
+        </div>
       </div>
     </div>
   );
