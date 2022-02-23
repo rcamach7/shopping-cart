61795d1e388984934909af7526d969cdb5935e5e
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-22 16:33:56 -0800

    add ability to filter products based on price and user preference

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -10,18 +10,56 @@ import {
   faArrowDown19,
   faArrowDown91,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Products(props) {
+  const [filteredProducts, setFilteredProducts] = useState(props.cars);
+
+  // If true, filters in ascending order, if false, filters in descending order.
+  const filterProducts = (ascending) => {
+    const filtered = [...filteredProducts];
+    if (ascending) {
+      filtered.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1;
+        }
+        if (a.price < b.price) {
+          return -1;
+        }
+        return 0;
+      });
+    } else {
+      filtered.sort((a, b) => {
+        if (a.price < b.price) {
+          return 1;
+        }
+        if (a.price > b.price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    setFilteredProducts(filtered);
+  };
+
   return (
     <main className="Products">
       <div className="filter-options">
         <p>Price filter: </p>
-        <FontAwesomeIcon icon={faArrowDown19} className="filter-option" />
-        <FontAwesomeIcon icon={faArrowDown91} className="filter-option" />
+        <FontAwesomeIcon
+          icon={faArrowDown91}
+          className="filter-option"
+          onClick={() => filterProducts(false)}
+        />
+        <FontAwesomeIcon
+          icon={faArrowDown19}
+          className="filter-option"
+          onClick={() => filterProducts(true)}
+        />
       </div>
 
       <div className="products-container">
-        {props.cars.map((car, i) => (
+        {filteredProducts.map((car, i) => (
           <CarProduct key={i} car={car} updateCart={props.updateCart} />
         ))}
       </div>
